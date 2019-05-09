70a9942afc2bdf3746ba2659a2c39f7de0247b76
unknown <joanthanmormar310@gmail.com>
2019-05-08 20:14:02 -0500

    segundo punto trabajo

---
+++ main.js
@@ -0,0 +1,3 @@
+console.log("Prueba - 1")
+console.log("Prueba - 2")
+console.log("Prueba - 3")sdfsdslñsdfkl
---
+++ main2.js
@@ -0,0 +1,3 @@
+console.log("Prueba - 2 - 1")
+console.log("Prueba - 2 - 2")
+console.log("Prueba - 2 - 3")
